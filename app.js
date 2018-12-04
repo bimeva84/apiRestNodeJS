486ae1fe04e990dcd08c772b7600b2cdb5b25fe4
Camilo Rojas Gomez <carojas@heinsohn.com.co>
2018-12-04 16:02:21 -0500

    demo POST Client

--- app.js
+++ app.js
@@ -1,21 +1,67 @@
 express = require("express"),
     app = express(),
     bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
+    methodOverride = require("method-override"),
     mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+const request = require("request");
 
 var router = express.Router();
 
-router.get('/saludosss', function(req, res) {
-   res.send("Hello World!");
+router.get('/saludo', function(req, res) {
+
+   request.post('http://localhost:8084/cyxteraweb/clients/', {
+    json: {
+        sharedKey: 'Jgutierrez',
+        businessId: 'Juan Gutierrez',
+        email: 'jgutierrez@netsecure.ci',
+        phone: '3106996767'
+    }
+  }, (error, resp, body) => {
+    if (error) {
+      console.error(error)
+      return
+    }else{
+        res.send(body.respuesta); 
+    }
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(body)
+  })
+
 });
 
 app.use(router);
 
-app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+  });
 });
+
+
+/**
+ * {
+"sharedKey": 'Jgutierrez',
+"businessId": 'Juan Gutierrez',
+"email": 'jgutierrez@netsecure.ci',
+"phone": '3106996767'
+}   
+ */
+
+var post_options = {
+    hostname: 'localhost',
+    port: 8084,
+    path: '/cyxteraweb/clients',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+  };
+  
+  
